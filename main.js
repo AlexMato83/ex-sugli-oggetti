0d7c10578a835f161dc9ac23058f5e71d0e2bacb
AlexMato <matontialessandro0@gmail.com>
2020-04-16 19:15:21 +0200

    secondo ex completato

--- main.js
+++ main.js
@@ -8,6 +8,26 @@ $(document).ready(function(){
   }
   console.log(palla);
   // salvo il peso inserito dall'utente e lo sostituisco a quello originale
-  palla.peso = parseInt(prompt("inserisci il peso"))
+  palla.peso = $("#insertWeight").val();
   console.log(palla);
+  $("#bottone").click(function(){
+    // se all'interno dell'input c'è un contenuto
+    if($("#insertWeight").val() != ""){
+      // creo dinamicamente un div per il titolo
+      $(".nome").prepend("<div>Questo è l'oggetto in questione:</div>")
+      // appendo al div esistente il nome
+      $(".nome").append("nome: " + palla.nome)
+      // appendo al div esistente il peso
+      $(".peso").append("peso: " + palla.peso)
+    }
+
+  })
+
+
+
+
+  // $("body").append(palla);
+
+
+
 });
