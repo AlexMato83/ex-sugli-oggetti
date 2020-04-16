0ebdb6dc32b3111359a31b25cdc6925562d2f65f
AlexMato <matontialessandro0@gmail.com>
2020-04-16 15:17:42 +0200

    primo ex ottimizzato

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-// Creare un oggetto palla che abbia le seguenti proprietà. Nome = palla Peso = 10 
-// Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla. 
+
 
 $(document).ready(function(){
-
+  // creo l'oggetto "palla"
   var palla = {
     "nome": "palla",
     "peso": 10
   }
- console.log(palla);
- var pesoInserito = parseInt(prompt("inserisci peso"));
-console.log(pesoInserito);
-palla.peso = pesoInserito;
-console.log(palla);
-
+  console.log(palla);
+  // salvo il peso inserito dall'utente e lo sostituisco a quello originale
+  palla.peso = parseInt(prompt("inserisci il peso"))
+  console.log(palla);
 });
